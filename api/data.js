63dbe3085c3b818e6a13807f003270c6eea95acf
Secret333Boy <pashkovskiy.eugen@gmail.com>
2022-01-03 06:04:31 +0200

    npm script fix

--- api/data.js
+++ api/data.js
@@ -1,4 +1,3 @@
-const jwt = require('jsonwebtoken');
 module.exports = (req, res) => {
   try {
     const token = req.headers.auth;
@@ -6,7 +5,7 @@ module.exports = (req, res) => {
       res.status(401).send('Unauthorized');
       return;
     }
-    const tokenData = jwt.verify(token, process.env.secretAccessJWTKey);
+
     res.status(200).json({ data: 'Secret data' });
   } catch (e) {
     res.status(500).json(e);
